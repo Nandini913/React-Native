8b337080efd59f4f672ff84404a17867344cbfca
NandiniTrivedi <nandini.trivedi@noovosoft.com>
2023-12-26 17:18:22 +0530

    Navigate to user on pressing

--- ActivityScreens/Contacts/Contact.tsx
+++ ActivityScreens/Contacts/Contact.tsx
@@ -1,30 +1,37 @@
-import {View, Text, Image} from 'react-native';
+import {View, Text, Image, Pressable} from 'react-native';
 import React from 'react';
 import {TUsers} from '../Home.tsx';
 import {styles} from '../../Stylesheet/StyleSheet.tsx';
+import {useNavigation} from '@react-navigation/native';
 type TContactProps = {
   user: TUsers;
 };
 
 function Contact({user}: TContactProps) {
+  const navigation = useNavigation();
+  const navigateToUser = () => {
+    navigation.navigate('UserProfile', {user: user});
+  };
   return (
-    <View style={styles.contact}>
-      <View style={styles.imageContainer}>
-        <Image
-          source={{
-            uri: user.image,
-          }}
-          style={styles.image}
-        />
+    <Pressable onPress={navigateToUser}>
+      <View style={styles.contact}>
+        <View style={styles.imageContainer}>
+          <Image
+            source={{
+              uri: user.image,
+            }}
+            style={styles.image}
+          />
+        </View>
+        <View style={styles.userDetail}>
+          <Text>
+            Name : {user.firstName} {user.lastName}
+          </Text>
+          <Text>Age : {user.age}</Text>
+          <Text>Phone : {user.phone}</Text>
+        </View>
       </View>
-      <View style={styles.userDetail}>
-        <Text>
-          Name : {user.firstName} {user.lastName}
-        </Text>
-        <Text>Age : {user.age}</Text>
-        <Text>Phone : {user.phone}</Text>
-      </View>
-    </View>
+    </Pressable>
   );
 }
 
