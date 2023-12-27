ccd72db1e3795fb9a64f51ce19a29701964ae0cc
NandiniTrivedi <nandini.trivedi@noovosoft.com>
2023-12-27 17:41:57 +0530

    initial commit

---
+++ NewExpoApp/App.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import {View, Text} from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+        <Text>You can start editing App.tsx </Text>
+    </View>
+  );
+}
